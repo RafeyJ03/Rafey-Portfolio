06563c898f498c255455d5c89a9022b623261876
RafeyJ03 <abdulrafey03@gmail.com>
2025-07-11 18:57:37 -0400

    improve SEO indicators

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://rafey-jawaid.com', // Replace with your real domain
+    generateRobotsTxt: true,
+    generateIndexSitemap: false,
+  };
