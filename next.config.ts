e4d221c165a09874e3948624245fc3721a027b77
RafeyJ03 <abdulrafey03@gmail.com>
2025-07-11 03:26:56 -0400

    vars to amplify runtim

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
+    RESEND_FROM: process.env.RESEND_FROM,
+    RESEND_TO: process.env.RESEND_TO,
+  },
 };
 
 export default nextConfig;
